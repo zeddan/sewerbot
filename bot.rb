0c470ac37d7911201ed4a96e4deb9bdfe2f17102
Robin Sääf <robin.saaf@chas.se>
2017-10-13 21:11:55 +0200

    Make bot listen to events

--- bot.rb
+++ bot.rb
@@ -1,37 +1,19 @@
 require 'slack-ruby-client'
+require 'slack-ruby-bot'
 require './scraper'
 require 'json'
 
-class Bot
-  # def initialize
-  #   $API_KEY = "xoxb-256033069970-HqH6rkCM2xtk1IiEiolNiMyW"
-  #   
-  #   Slack.configure do |config|
-  #     #config.token = ENV['SLACK_API_TOKEN']
-  #     config.token = $API_KEY;
-  #   end
+class Bot < SlackRubyBot::Bot
 
-  #   s = Scraper.new()
-  #   s.scrape
-  #   s.compare_old
-  #   s.compare_new
-  #   arr = s.get_new
-  #   
-  #   @client = Slack::Web::Client.new
-  #   @client.auth_test
-  #   @client.chat_postMessage(channel: '#dev', text: arr.to_json, as_user: true)
-  # end
-
-  def initialize
-    client = Slack::RealTime::Client.new
-
-    client.on :hello do
-      puts "okasd"
-    end
-
-    client.start!
+  command 'foo' do |client, data, match|
+    s = Scraper.new()
+    s.scrape
+    s.compare_old
+    s.compare_new
+    arr = s.get_new
+    client.say(text: arr.to_json, channel: data.channel)
   end
 
 end
 
-bot = Bot.new
+Bot.run
